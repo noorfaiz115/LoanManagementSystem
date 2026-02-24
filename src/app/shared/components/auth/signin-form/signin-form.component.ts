d25b80ed90d270087e3332c8e88a9b5be16634c5
Faiz Noor <noorfaiz115@gmail.com>
2026-02-24 14:15:45 +0530

    Add AuthModule

--- src/app/shared/components/auth/signin-form/signin-form.component.ts
+++ src/app/shared/components/auth/signin-form/signin-form.component.ts
@@ -1,40 +1,58 @@
-
-import { Component } from '@angular/core';
-import { LabelComponent } from '../../form/label/label.component';
-import { CheckboxComponent } from '../../form/input/checkbox.component';
-import { ButtonComponent } from '../../ui/button/button.component';
-import { InputFieldComponent } from '../../form/input/input-field.component';
-import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-
+import { Component } from "@angular/core";
+import { LabelComponent } from "../../form/label/label.component";
+import { CheckboxComponent } from "../../form/input/checkbox.component";
+import { ButtonComponent } from "../../ui/button/button.component";
+import { InputFieldComponent } from "../../form/input/input-field.component";
+import { Router, RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import { AuthService } from "../../../services/auth.service";
 @Component({
-  selector: 'app-signin-form',
+  selector: "app-signin-form",
   imports: [
     LabelComponent,
     CheckboxComponent,
     ButtonComponent,
     InputFieldComponent,
     RouterModule,
-    FormsModule
-],
-  templateUrl: './signin-form.component.html',
-  styles: ``
+    FormsModule,
+  ],
+  templateUrl: "./signin-form.component.html",
+  styles: ``,
 })
 export class SigninFormComponent {
-
   showPassword = false;
   isChecked = false;
 
-  email = '';
-  password = '';
+  email = "";
+  password = "";
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {}
 
   togglePasswordVisibility() {
     this.showPassword = !this.showPassword;
   }
 
   onSignIn() {
-    console.log('Email:', this.email);
-    console.log('Password:', this.password);
-    console.log('Remember Me:', this.isChecked);
+    const loginData = {
+      email: this.email,
+      password: this.password,
+    };
+    console.log("Login Data:", loginData);
+
+    this.authService.login(loginData).subscribe({
+      next: (response) => {
+        console.log("Login successful:", response);
+
+        localStorage.setItem("token", response.data.token);
+
+        this.router.navigate(["/"]);
+      },
+      error: (error) => {
+        console.error("Login failed:", error);
+        alert("Login failed. Please check your credentials and try again.");
+      },
+    });
   }
 }
