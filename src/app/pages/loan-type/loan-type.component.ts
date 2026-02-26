921fca745e3baf33537b7c2d23585b10acbb34a1
Faiz Noor <noorfaiz115@gmail.com>
2026-02-26 13:45:36 +0530

    Faizside

--- src/app/pages/loan-type/loan-type.component.ts
+++ src/app/pages/loan-type/loan-type.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
+import { BasicTableFourComponent } from "../../shared/components/tables/basic-tables/basic-table-four/basic-table-four.component";
+import { LoanTypeService } from "../../shared/services/loan-type.service";
 
 @Component({
-  selector: 'app-loan-type',
-  imports: [],
-  templateUrl: './loan-type.component.html',
-  styleUrl: './loan-type.component.css',
+  selector: "app-loan-type",
+  imports: [BasicTableFourComponent],
+  templateUrl: "./loan-type.component.html",
+  styleUrl: "./loan-type.component.css",
 })
 export class LoanTypeComponent {
+  constructor(private loan: LoanTypeService) {
+    this.loadAllType();
+  }
+  loanType: any[] = [];
 
+  loadAllType() {
+    return this.loan.GetLoanTypes().subscribe((res) => {
+      this.loanType = res.data;
+      console.log(this.loanType);
+    });
+  }
 }
--- src/app/shared/components/tables/basic-tables/basic-table-five/basic-table-five.component.ts
+++ src/app/shared/components/tables/basic-tables/basic-table-five/basic-table-five.component.ts
@@ -1,5 +1,4 @@
-import { Component } from "@angular/core";
-
+import { Component, input, Input } from "@angular/core";
 @Component({
   selector: "app-basic-table-five",
   imports: [],
@@ -7,65 +6,7 @@ import { Component } from "@angular/core";
   styles: ``,
 })
 export class BasicTableFiveComponent {
-  tableData = [
-    {
-      id: 1,
-      name: "TailGrids",
-      Gender: "Male",
-      category: "UI Kits",
-      country: "/images/country/country-01.svg",
-      cr: "Dashboard",
-      value: "12,499",
-    },
-    {
-      id: 2,
-      name: "GrayGrids",
-      category: "Templates",
-      country: "/images/country/country-02.svg",
-      cr: "Dashboard",
-      value: "5498",
-    },
-    {
-      id: 3,
-      name: "Uideck",
-      category: "Templates",
-      country: "/images/country/country-03.svg",
-      cr: "Dashboard",
-      value: "4621",
-    },
-    {
-      id: 4,
-      name: "FormBold",
-      category: "SaaS",
-      country: "/images/country/country-04.svg",
-      cr: "Dashboard",
-      value: "13843",
-    },
-    {
-      id: 5,
-      name: "NextAdmin",
-      category: "Templates",
-      country: "/images/country/country-05.svg",
-      cr: "Dashboard",
-      value: "7523",
-    },
-    {
-      id: 6,
-      name: "Form Builder",
-      category: "Templates",
-      country: "/images/country/country-06.svg",
-      cr: "Dashboard",
-      value: "1,377",
-    },
-    {
-      id: 7,
-      name: "AyroUI",
-      category: "Templates",
-      country: "/images/country/country-07.svg",
-      cr: "Dashboard",
-      value: "599,00",
-    },
-  ];
+  @Input() tableData: any[] = [];
 
   handleFilter() {
     console.log("Filter clicked");
